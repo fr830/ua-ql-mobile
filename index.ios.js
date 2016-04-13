37644c61c1714c171c818cc7db9a836b957c5cf4
Giles <giles.bradshaw@sigyl.com>
2016-04-13 22:15:41 +0100

    trimed idex files

--- index.ios.js
+++ index.ios.js
@@ -3,14 +3,8 @@
  * https://github.com/facebook/react-native
  */
 
-import React, {
+import {
   AppRegistry,
-  Component,
-  Image,
-  StyleSheet,
-  Text,
-  View,
-  ListView,
 } from 'react-native';
 
 import Relay from 'react-relay';
@@ -24,107 +18,5 @@ Relay.injectNetworkLayer(
   new Relay.DefaultNetworkLayer('https://ua-ql.herokuapp.com/graphql')
 );
 
-/**
- * For quota reasons we replaced the Rotten Tomatoes' API with a sample data of
- * their very own API that lives in React Native's Github repo.
- */
-const REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
-
-class AwesomeProject extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2,
-      }),
-      loaded: false,
-    };
-  }
-  componentDidMount() {
-    this.fetchData();
-  }
-  fetchData() {
-    fetch(REQUEST_URL)
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.setState({
-          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
-          loaded: true,
-        });
-      })
-      .done();
-  }
-  render() {
-    if (!this.state.loaded) {
-      return this.renderLoadingView();
-    }
-
-    return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderMovie}
-        style={styles.listView}
-      />
-    );
-  }
-
-  renderLoadingView() {
-    return (
-      <View style={styles.container}>
-        <MyApp/>
-        <Text>
-          Loading movies...
-        </Text>
-      </View>
-    );
-  }
-
-  renderMovie(movie) {
-    return (
-      <View style={styles.container}>
-        <Text
-          source={{uri: movie.posters.thumbnail}}
-          style={styles.thumbnail}
-        >ok</Text>
-        <View style={styles.rightContainer}>
-          <Text style={styles.title}>ok</Text>
-          <Text style={styles.year}>ok</Text>
-        </View>
-      </View>
-    );
-  }
-
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex:1,
-    backgroundColor: 'green',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  rightContainer: {
-    flex: 1,
-  },
-  thumbnail: {
-    width: 53,
-    height: 81,
-  },
-  title: {
-    fontSize: 20,
-    marginBottom: 8,
-    textAlign: 'center',
-  },
-  year: {
-    textAlign: 'center',
-  },
-  listView: {
-    paddingTop: 10,
-    paddingBottom: 10,
-    backgroundColor: 'red',
-  }
-  
-});
 
 AppRegistry.registerComponent('AwesomeProject', () => MyApp);
